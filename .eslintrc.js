17cdbf2a04496e85207635b0c21fb77850726c47
domingo2000 <domingo.edwards@uc.cl>
2022-10-03 17:57:44 -0300

    Fix(webpack-eslint-parser): Fixed problem with eslint-parser that throwed bug in CD worlflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   env: {
     browser: true,
+    node: true,
     es2021: true,
     'jest/globals': true
   },
   extends: [
     'standard'
   ],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
