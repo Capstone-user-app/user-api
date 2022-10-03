002dc86f76b4f2901bc7f059fa2e0daab01c0d2f
domingo2000 <domingo.edwards@uc.cl>
2022-10-03 14:52:06 -0300

    Feature(sales-mock-data): Added brand name and logo, also added more items to mock data to provide more states

--- src/functions/sales_crud/salesMockData.js
+++ src/functions/sales_crud/salesMockData.js
@@ -1,84 +1,301 @@
-const salesMockData = [{
-  packageId: 'de933a81-3549-47f8-a21e-3d78f6aa8764',
-  pinflagId: 'y676au',
-  saleOrder: '001234567',
-  packageStatus: 7,
-  state: 'Valparaíso',
-  stateId: 5,
-  city: 'El Tabo',
-  cityId: 5605,
-  clickAndCollect: true,
-  evidencePhoto: null,
-  height: 12,
-  width: 12,
-  length: 30,
-  position: null,
-  warehouseName: 'Bodega testing',
-  warehouseId: 15,
-  deliveryAddress: 'La direccion 1234',
-  deliveryInstructions: null,
-  clientEmail: 'g4-capstone@pinflag.cl',
-  clientName: 'Obiwan Kenobi',
-  clientLastName: '',
-  clientPhone: '+56911223344',
-  products: [
-    {
-      productName: 'Polera roja',
-      sku: '1111',
-      price: 1222,
-      weight: 12,
-      photoUrl: [
-        {
-          URLIMAGEN: 'https://via.placeholder.com/200x120.png'
-        }
-      ]
-    },
-    {
-      productName: 'Polera negra',
-      sku: '1112',
-      price: 1222,
-      weight: 12,
-      photoUrl: [
-        {
-          URLIMAGEN: 'https://via.placeholder.com/200x120.png'
-        },
-        {
-          URLIMAGEN: 'https://via.placeholder.com/200x120.png'
-        }
-      ]
-    }
-  ],
-  packageHistory: [
-    {
-      packageStatus: 1,
-      date: '2022-03-11T02:27:35.243Z'
-    },
-    {
-      packageStatus: 2,
-      date: '2022-03-11T02:30:26.493Z'
-    },
-    {
-      packageStatus: 3,
-      date: '2022-03-11T02:31:25.020Z'
-    },
-    {
-      packageStatus: 4,
-      date: '2022-03-11T02:35:56.873Z'
-    },
-    {
-      packageStatus: 7,
-      date: '2022-03-11T02:38:37.067Z'
-    }
-  ],
-  trackingNumber: null,
-  courier: 'bluexpress',
-  shippingCost: {
-    price: 125,
-    courier: 'bluexpress',
-    delivery_time: '2-5 días hábiles'
+const salesMockData = [
+  {
+    packageId: 'd56ef70e-39f9-46c8-9abd-0008a11d4752',
+    pinflagId: 'i2irei',
+    saleOrder: 'PINTEST#INT398694',
+    ecommerce: 'Lippi',
+    imageEcommerce: 'https://www.lippioutdoor.com/static/version1664570782/frontend/Linets/Lippi/es_AR/images/logo.png',
+    packageStatus: 1,
+    state: 'Metropolitana de Santiago',
+    stateId: 13,
+    city: 'Providencia',
+    cityId: 13123,
+    clickAndCollect: true,
+    evidencePhoto: null,
+    height: null,
+    width: null,
+    length: null,
+    position: null,
+    warehouseName: 'Pick Up Blue Express Tecnobox Ingles 2',
+    warehouseId: 35,
+    deliveryAddress: 'El gobernador 20, 201',
+    deliveryInstructions: null,
+    clientEmail: 'ajavier.ruiz@pinflag.cl',
+    clientName: 'Nicolas',
+    clientLastName: 'Raimman',
+    clientPhone: '959333263',
+    products: [
+      {
+        productName: 'Chaqueta Hombre Paicavi Therm-Pro Jacket Negro Lippi',
+        sku: '1637202004113',
+        price: 54990,
+        weight: 1,
+        photoUrl: [
+          {
+            URLIMAGEN: 'https://res-3.cloudinary.com/lippi-outdoor/image/upload/c_limit,dpr_2.0,f_auto,q_auto/v1/media/catalog/product/m/_/m_paicav_therm-pro_jacket._negro._1.jpg'
+          }
+        ]
+      }
+    ],
+    totalProductsPrice: 54990,
+    packageHistory: [
+      {
+        packageStatus: 1,
+        date: '2022-05-14T03:43:50.280Z'
+      }
+    ],
+    trackingNumber: null,
+    shippingCost: null,
+    shippingPaid: null,
+    incident: null
   },
-  cost: 11111
-}
+  {
+    packageId: '1b2ed3f9-8b3c-4779-8ef7-00ab81bca5c3',
+    pinflagId: '8vcrhl',
+    saleOrder: '0008',
+    ecommerce: 'Lippi',
+    imageEcommerce: 'https://www.lippioutdoor.com/static/version1664570782/frontend/Linets/Lippi/es_AR/images/logo.png',
+    packageStatus: 2,
+    state: 'Valparaíso',
+    stateId: 5,
+    city: 'El Tabo',
+    cityId: 5605,
+    clickAndCollect: true,
+    evidencePhoto: null,
+    height: 12,
+    width: 12,
+    length: 30,
+    position: '0010202',
+    warehouseName: 'Costanera Lippi',
+    warehouseId: 1,
+    deliveryAddress: 'Andrés Bello 2425',
+    deliveryInstructions: null,
+    clientEmail: 'tonyparker@gmail.com',
+    clientName: 'Tony',
+    clientLastName: 'Parker',
+    clientPhone: '+56 9 882 281 83',
+    products: [
+      {
+        productName: 'Primera Capa Hombre Skintec 2000 Seamless Top Negro / Grafito',
+        sku: 'TX806M021172101U',
+        price: 26990,
+        weight: 12,
+        photoUrl: [
+          {
+            URLIMAGEN: 'https://www.lippioutdoor.com/media/catalog/product/cache/2f56a79289bf75fa30c9498eece5092d/c/6/c612.jpg'
+          }
+        ]
+      }
+    ],
+    totalProductsPrice: 26990,
+    packageHistory: [
+      {
+        packageStatus: 4,
+        date: '2021-07-09T21:24:03.493Z'
+      },
+      {
+        packageStatus: 5,
+        date: '2021-07-09T21:24:15.303Z'
+      },
+      {
+        packageStatus: 2,
+        date: '2021-12-10T20:21:53.700Z'
+      }
+    ],
+    trackingNumber: null,
+    courier: 'bluexpress',
+    shippingCost: 5290,
+    shippingPaid: null,
+    incident: null
+  },
+  {
+    packageId: 'a80f5968-1309-4020-bac1-00ae98958ea2',
+    pinflagId: 'iuy2i6',
+    saleOrder: 'JOCHE - LIPPI - CC3',
+    ecommerce: 'Lippi',
+    imageEcommerce: 'https://www.lippioutdoor.com/static/version1664570782/frontend/Linets/Lippi/es_AR/images/logo.png',
+    packageStatus: 3,
+    state: 'Metropolitana de Santiago',
+    stateId: 13,
+    city: 'Providencia',
+    cityId: 13123,
+    clickAndCollect: true,
+    evidencePhoto: 'https://myfflagbucket.s3.sa-east-1.amazonaws.com/A80F5968-1309-4020-BAC1-00AE98958EA2-2022-03-22.jpg%20/%20png%20/%20jpeg',
+    height: null,
+    width: null,
+    length: null,
+    position: '0010101',
+    warehouseName: 'Bodega testing',
+    warehouseId: 15,
+    deliveryAddress: 'El gobernador 20, 201',
+    deliveryInstructions: null,
+    clientEmail: 'javier.ruiz@pinflag.cl',
+    clientName: 'Javier',
+    clientLastName: 'Ruiz',
+    clientPhone: '959333263',
+    products: [
+      {
+        productName: 'The Full Set',
+        sku: '1637202004113',
+        price: 99990,
+        weight: 1,
+        photoUrl: [
+          {
+            URLIMAGEN: 'https://cdn.shopify.com/s/files/1/0587/5158/2374/products/Thefullset.jpg?v=1640647083'
+          }
+        ]
+      }
+    ],
+    totalProductsPrice: 99990,
+    packageHistory: [
+      {
+        packageStatus: 1,
+        date: '2022-03-10T05:36:16.203Z'
+      },
+      {
+        packageStatus: 4,
+        date: '2022-03-10T05:37:08.147Z'
+      },
+      {
+        packageStatus: 7,
+        date: '2022-03-10T05:37:31.283Z'
+      },
+      {
+        packageStatus: 5,
+        date: '2022-03-23T03:14:51.087Z'
+      },
+      {
+        packageStatus: 6,
+        date: '2022-03-23T03:17:19.553Z'
+      }
+    ],
+    trackingNumber: null,
+    shippingCost: null,
+    shippingPaid: null,
+    incident: null
+  },
+  {
+    packageId: '9f84e2e9-13c1-40d0-8c7c-1709b0598089',
+    pinflagId: 'd7hxwb',
+    saleOrder: '2063',
+    ecommerce: 'Lippi',
+    imageEcommerce: 'https://www.lippioutdoor.com/static/version1664570782/frontend/Linets/Lippi/es_AR/images/logo.png',
+    packageStatus: 4,
+    state: 'Libertador General Bernardo OHiggins',
+    stateId: 6,
+    city: 'Rancagua',
+    cityId: 6101,
+    clickAndCollect: true,
+    evidencePhoto: null,
+    height: null,
+    width: null,
+    length: null,
+    position: null,
+    warehouseName: 'Bodega testing',
+    warehouseId: 15,
+    deliveryAddress: 'Pintor Gustavo Cabello (Ex Calle 5) 835, 305',
+    deliveryInstructions: null,
+    clientEmail: 'oscar.carcamo@pinflag.cl',
+    clientName: 'Jose Ignacio',
+    clientLastName: 'Lopez',
+    clientPhone: '988393296',
+    products: [
+      {
+        productName: 'The Basic Set',
+        sku: '1637202698914',
+        price: 53990,
+        weight: 1,
+        photoUrl: [
+          {
+            URLIMAGEN: 'https://cdn.shopify.com/s/files/1/0587/5158/2374/products/SETTHEBASIC_af5aef0a-c46d-472b-b48f-f5f87fc3b3b0.jpg?v=1637056877'
+          }
+        ]
+      }
+    ],
+    totalProductsPrice: 53990,
+    packageHistory: [
+      {
+        packageStatus: 1,
+        date: '2022-05-23T22:57:43.827Z'
+      }
+    ],
+    trackingNumber: null,
+    courier: 'bluexpress',
+    shippingCost: 2690,
+    shippingPaid: null,
+    incident: null
+  },
+  {
+    packageId: '147f6143-21c2-467b-8d31-0153c94e9911',
+    pinflagId: '1siacc',
+    saleOrder: null,
+    ecommerce: 'Lippi',
+    imageEcommerce: 'https://www.lippioutdoor.com/static/version1664570782/frontend/Linets/Lippi/es_AR/images/logo.png',
+    packageStatus: 7,
+    state: 'Valparaíso',
+    stateId: 5,
+    city: 'El Tabo',
+    cityId: 5605,
+    clickAndCollect: true,
+    evidencePhoto: null,
+    height: 12,
+    width: 10,
+    length: 30,
+    position: '0010202',
+    warehouseName: 'Costanera Lippi',
+    warehouseId: 1,
+    deliveryAddress: 'Av. Andrés Bello 2425',
+    deliveryInstructions: null,
+    clientEmail: 'guajardorosa33@gmail.com',
+    clientName: 'Rosa Guajardo Fuentes ',
+    clientLastName: '',
+    clientPhone: null,
+    products: [
+      {
+        productName: 'Zapatilla Mujer Haka Honu Aloha Negro',
+        sku: '531611707569V019',
+        price: 34990,
+        weight: 1,
+        photoUrl: [
+          {
+            URLIMAGEN: 'https://www.lippioutdoor.com/media/catalog/product/2/3/23_1_14.jpg'
+          }
+        ]
+      }
+    ],
+    totalProductsPrice: 34990,
+    packageHistory: [
+      {
+        packageStatus: 1,
+        date: '2021-09-01T11:01:07.950Z'
+      },
+      {
+        packageStatus: 2,
+        date: '2021-09-01T11:01:08.637Z'
+      },
+      {
+        packageStatus: 3,
+        date: '2021-09-01T22:01:07.273Z'
+      },
+      {
+        packageStatus: 4,
+        date: '2021-09-02T02:18:03.870Z'
+      },
+      {
+        packageStatus: 5,
+        date: '2021-09-02T02:18:12.867Z'
+      },
+      {
+        packageStatus: 6,
+        date: '2021-09-08T00:48:13.030Z'
+      }
+    ],
+    trackingNumber: null,
+    courier: 'bluexpress',
+    shippingCost: 2690,
+    shippingPaid: null,
+    incident: null
+  }
 ]
 
 export { salesMockData }
